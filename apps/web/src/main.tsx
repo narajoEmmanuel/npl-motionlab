611b72bad6893f960582d77823473d0bc6b323bc
Emmanuel Naranjo <101431425+narajoEmmanuel@users.noreply.github.com>
2026-09-17 11:29:22 -0600

    feat: load results workspace styles

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -5,6 +5,7 @@ import App from "./App";
 import "./styles/tokens.css";
 import "./styles/app.css";
 import "./styles/review.css";
+import "./styles/results.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
